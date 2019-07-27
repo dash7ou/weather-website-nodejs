c4b80145b77dc1265fdae63f6b6fdcc656cf25a3
mohammed zourob <fxfddfno@gmail.com>
2019-07-27 17:44:22 +0300

    Some adjustment in css and dom js

--- public/js/app.js
+++ public/js/app.js
@@ -1,40 +1,50 @@
 console.log("Client side javascript file is loaded!");
 
-// const url = "http://localhost:3000/weather?address=Gaza";
-
-// fetch(url)
-//   .then(res => res.json())
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err));
-
 const weatherSearchForm = document.querySelector("form");
 const search = document.querySelector("input");
 const errorP = document.getElementById("error");
 const locationP = document.getElementById("location");
+const weatherP = document.getElementById("weatherNow");
+const dataOut = document.getElementById("returnData");
+
 weatherSearchForm.addEventListener("submit", e => {
   "use strick";
   e.preventDefault();
-  if (search.value == "") return console.log("Enter Address in Input ");
-  const location = search.value;
-  const url = `/weather?address=${location}`;
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      if (data.error) {
-        errorP.style.color = "red";
-        errorP.style.border = "1px solid #888";
-        errorP.style.padding = "10px";
-        errorP.textContent = data.error;
-      } else {
-        errorP.style.color = "blue";
-        //errorP.style.border = "1px solid #888";
-        errorP.style.padding = "10px 10px 0 10px";
-        errorP.textContent = data.error;
-        locationP.style.padding = "0px 10px 0 10px";
-        locationP.textContent = data.error;
-        locationP.style.color = "blue";
-        errorP.textContent = `${data.address} Weather: ${data.forecast}`;
-        locationP.textContent = `location: ${data.location}`;
-      }
-    });
+
+  if (search.value == undefined || search.value == "") {
+    dataOut.classList.add("deleteAny");
+    errorP.style.color = "red";
+    errorP.textContent = "Add any location to search.. ";
+  } else {
+    const location = search.value;
+
+    dataOut.classList.add("deleteAny");
+
+    const url = `/weather?address=${location}`;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        if (data.forecast) {
+          dataOut.classList.remove("deleteAny");
+          locationP.textContent = "Loading...";
+
+          dataOut.style.color = "blue";
+
+          errorP.classList.add("deleteAny");
+
+          weatherP.textContent = `${data.address} Weather: ${data.forecast}`;
+          locationP.textContent = `location: ${data.location}`;
+          dataOut.classList.add("error");
+        } else {
+          errorP.classList.remove("deleteAny");
+          errorP.textContent = "Loading...";
+          errorP.textContent = "Enter right location please";
+          errorP.style.color = red;
+        }
+      })
+      .catch(err => {
+        errorP.textContent = err;
+      });
+  }
 });
